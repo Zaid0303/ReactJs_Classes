1cac0fe936a2a02b93b5db0609e23764afc42947
asp <asp@aptech.com>
2025-05-31 13:16:16 +0500

    Functional Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import UncontrolledExample from './header.jsx';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <UncontrolledExample></UncontrolledExample>
+      <h1>Zaid</h1>
     </div>
   );
 }
